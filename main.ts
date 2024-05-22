671198278145f3b45a748669e8b710951d95256d
calmwaves <calmwaves@gitmail.com>
2024-05-22 16:20:46 +0800

    demo

--- main.ts
+++ main.ts
@@ -1,85 +1,91 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
+const DEFAULT_SETTINGS = {
+	anytxtApiUrl: 'http://127.0.0.1:9920'
+};
 
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class AnytxtSearchPlugin extends Plugin {
+	settings: any;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+		this.registerDomEvent(document, 'keydown', (evt: KeyboardEvent) => {
+			const target = evt.target as HTMLInputElement;
+			if (evt.key === 'Enter' && target.getAttribute('enterkeyhint') === 'search' && target.type === 'search') {
+				const keyword = target.value;
+				this.searchAnytxt(keyword);
 			}
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
-	onunload() {
+	async searchAnytxt(keyword: string) {
+		const apiURL = this.settings.anytxtApiUrl;
+        //@ts-ignore
+        const vaultBasePath = this.app.vault.adapter.basePath;
+		const searchParams = {
+			id: 123,
+			jsonrpc: "2.0",
+			method: "ATRpcServer.Searcher.V1.Search",
+			params: {
+				input: {
+					pattern: `"${keyword}"`,
+					filterDir: vaultBasePath,
+					// filterExt: "png",
+                    filterExt: "*",
+				}
+			}
+		};
 
+		const options = {
+			method: 'POST',
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json'
+			}
+		};
+
+		fetch(apiURL, {
+			method: 'POST',
+			headers: options.headers,
+			body: JSON.stringify(searchParams)
+		})
+		.then(response => response.json())
+		.then(data => {
+			if (data.error) {
+				new Notice('Anytxt API Error: ' + data.error.message, 10000);
+			} else {
+                // 创建一个Notice来显示匹配的文件数量
+                const notice1 = new Notice(`anytxt 匹配的文件数量: ${data.result.data.output.count}\n右键点击本通知打开anytxt`, 10000);
+            
+                // 右键点击通知，打开Anytxt
+                notice1.noticeEl.oncontextmenu = () => {
+                    // 创建一个命令行参数，用于启动Anytxt并搜索文件
+                    // const command = `"D:\\AnyTXT Searcher\\ATGUI.exe" /s "${keyword}" /d "${vaultBasePath}" /e png`;
+                    const command = `"D:\\AnyTXT Searcher\\ATGUI.exe" /s "${keyword}" /d "${vaultBasePath}"`;
+
+                    console.log(command);
+            
+                    // 执行命令
+                    require('child_process').exec(command, (error: Error, stdout: string, stderr: string) => {
+                        if (error) {
+                            // 如果有错误，显示错误消息
+                            new Notice('命令执行错误: ' + error.message, 10000);
+                        } else {
+                            // 如果没有错误，可以在这里添加处理stdout和stderr的逻辑
+                        }
+                    });
+                };
+            }
+            
+		})
+		.catch(error => {
+            if (error.message === 'Failed to fetch') {
+                new Notice('无法连接到 Anytxt API 服务器，请运行anytxt.exe', 10000);
+            }else {
+            new Notice('请求过程中发生错误: ' + error.message, 10000);
+            }
+		});
 	}
 
 	async loadSettings() {
@@ -91,26 +97,10 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class AnytxtSearchSettingTab extends PluginSettingTab {
+	plugin: AnytxtSearchPlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: AnytxtSearchPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -121,13 +111,13 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Anytxt API URL')
+			.setDesc('The URL of the Anytxt search API.')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('Enter API URL')
+				.setValue(this.plugin.settings.anytxtApiUrl)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.anytxtApiUrl = value;
 					await this.plugin.saveSettings();
 				}));
 	}
